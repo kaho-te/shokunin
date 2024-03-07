07fa654f47f21c61a06f33642ab845e29af4a49d
kaho-te <kaho.t1031@gmail.com>
2024-03-07 16:56:20 +0900

    パッケージ詳細のデータ取得

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Type::create([
+            'name' => 'NIHONSHU',
+            'icon' => 'icon_dummy',
+            'image' => 'dummy',
+        ]);
+        Type::create([
+            'name' => 'GEIKO',
+            'icon' => 'icon_dummy',
+            'image' => 'dummy',
+
+        ]);
+        Type::create([
+            'name' => 'MAIKO',
+            'icon' => 'icon_dummy',
+            'image' => 'dummy',
+
+        ]);
+        Type::create([
+            'name' => 'SHAMISEN',
+            'icon' => 'icon_dummy',
+            'image' => 'dummy',
+        ]);
+    }
+}
