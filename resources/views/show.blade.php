afaf258cd3406b3fd57af713debc7afbbc21ac07
kaho-te <kaho.t1031@gmail.com>
2024-03-07 16:19:00 +0900

    パッケージ詳細のデータ取得

---
+++ resources/views/show.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+  <div class="px-5 py-2">
+    <h1 class="text-xl font-bold">{{ $package->name }}</h1>
+    <div class="text-xs">開催期間： {{ $package->start_date }} 〜 {{ $package->end_date }}</div>
+    @foreach ($artisanTypes as $artisanType)
+      <div>
+        <div><img src={{ $artisanType->image }} alt=""></div>
+        <div>
+          <div>{{ $artisanType->name }}</div>
+        </div>
+      </div>
+    @endforeach
+    <div class="text-xs">adult {{ $package->adult_price }} 円（child {{ $package->child_price }}円）</div>
+    <img src={{ $package->image }} alt="パッケージ画像" width=350 height=200>
+    <div class="my-2"><img src="dummy.png"><span class="text-xs font-bold">WOW</span></div>
+    <div class="text-xs break-words whitespace-pre-wrap">{!! nl2br(e($package->description)) !!}</div>
+    <div id="content" class="mx-auto">
+      <h2 class="text-base font-bold text-center">Content</h2>
+      <div class="text-xs leading-6">{!! nl2br(e($package->plan)) !!}</div>
+    </div>
+    <div class="reserve_btn my-12 text-center">
+      <button onclick="location.href='{{ route('shokunin.index') }}'"  class="px-16 py-3">Reserve</button>
+    </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        @foreach ($package->get_artisans as $artisan)
+          <div>
+            <div><img src={{ $artisan->image }} alt=""></div>
+            <div>
+              <div class="text-base font-bold">{{ $artisan->name }}</div>
+              <p class="text-xs break-words whitespace-pre-wrap">{{ $artisan->description }}</p>
+              <button class="text-xs" onclick="location.href='dummy' ">detail</button>
+            </div>
+          </div>
+        @endforeach
+        <div class="reserve_btn my-12 text-center">
+          <button onclick="location.href='{{ route('shokunin.index') }}'"  class="px-16 py-3">Reserve</button>
+        </div>
+    </div>
+  </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('packages', PackageController::class);
+    Route::resource('shokunin', PackageController::class);
     Route::get('/packages/search', [PackageController::class, 'search'])->name('packages.search');
     Route::get('/packages/check', [PackageController::class, 'check'])->name('packages.check');
     Route::get('reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
     Route::post('reservations/store', [ReservationController::class, 'store'])->name('reservations.store');
     Route::get('reservations/check', [ReservationController::class, 'check'])->name('reservations.check');
+    // 検証用 
+    // Route::get('/packages/show', [PackageController::class, 'show'])->name('packages.show');
 });
 
 require __DIR__ . '/auth.php';
